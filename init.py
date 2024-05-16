77f6dca8127ef1882789f8195e8b12e64ad46c4c
Andrew Hoang <andrew@Andrews-MacBook-Pro.local>
2024-05-16 11:42:57 -0400

    general structure of how to use the model for inference

--- init.py
+++ init.py
@@ -1,7 +1,11 @@
 # model_initializer.py
+#note install with:
+#pip install git+https://github.com/huggingface/transformers.git sentencepiece
+
 from transformers import SeamlessM4Tv2Model, AutoProcessor
 import torch
-import torchaudio
+import scipy
+import sounddevice as sd
 #print(torch.__version__) version 2.3.0
 
 
@@ -18,11 +22,15 @@ def initialize_model():
         print("running on cpu")
     processor = AutoProcessor.from_pretrained("facebook/seamless-m4t-v2-large")
     model = SeamlessM4Tv2Model.from_pretrained("facebook/seamless-m4t-v2-large")
-    model = model.to(device)
+    #model = model.to(device)
+    model = model.to(torch.device("cpu"))
     return model, processor, device
 
 model, processor, device = initialize_model()
 
 #from text
 text_inputs = processor(text = "Hello, my name is andrew and I am graduating from college", src_lang="eng", return_tensors="pt")
-audio_array_from_text = model.generate(**text_inputs, tgt_lang="rus")[0].cpu().numpy().squeeze()
+audio_array_from_text = model.generate(**text_inputs, tgt_lang="hin")[0].cpu().numpy().squeeze()
+
+sample_rate = model.config.sampling_rate
+scipy.io.wavfile.write("out_from_text.wav", rate=sample_rate, data=audio_array_from_text)
